5a0bcceaa26b1a26a2bff827fdaa9dd65e55a77e
matheussc <243501@dac.unicamp.br>
2025-04-10 15:42:19 -0300

    mudancas do ambiente, adicionando lista de obstaculos e trocando o detectar limites para utilizar altura do ambiente

--- lab03/Ambiente.java
+++ lab03/Ambiente.java
@@ -4,30 +4,49 @@ public class Ambiente
 {
     private int largura;
     private int comprimento;
+    private int altura;
     private ArrayList<Robo> robos;
+    private ArrayList<Obstaculo> obstaculos;
+
     public Ambiente(int l, int c)
     {
         largura = l;
         comprimento = c;
         robos = new ArrayList<>();
     }
+
     public boolean dentroDosLimites(Robo r)
     {
         if(r != null){
             if (r instanceof RoboAereo ra) 
-                return (ra.getPosX() <= this.largura && ra.getPosY() <= this.comprimento && ra.getPosZ() <= ra.getAltitudeMaxima());
+                return (ra.getPosX() <= this.largura && ra.getPosY() <= this.comprimento && ra.getPosZ() <= this.altura);
             
             return (r.getPosX() <= this.largura && r.getPosY() <= this.comprimento);
         }
         else
             return false;
     }
-    public void adicionarRobo(Robo r)
-    {
+
+    //robos
+    public void adicionarRobo(Robo r) {
         this.robos.add(r);
+    }
+    public void removerRobo(Robo r){
+        this.robos.remove(r);
     }
     public ArrayList<Robo> getRobos(){
         return this.robos;
+    }
+
+    //obstaculos
+    public void adicionarObstaculo(Obstaculo ob){
+        this.obstaculos.add(ob);
+    }
+    public void removerObstaculo(Obstaculo ob){
+        this.obstaculos.remove(ob);
+    }
+    public ArrayList<Obstaculo> getObstaculos(){
+        return this.obstaculos;
     }
 
     public void imprimirDimensoes(){
