fc95b95945ec0227a111b019d50308f30e1effe5
Eduardo Moura Zampirolli <ra281154@pernalonga.lab.ic.unicamp.br>
2025-06-11 12:57:01 -0300

    adicionei exceptions para o mover do controle de movimento, mas ainda tem que verificar como usar a movimentação na coordenada z

--- lab05/subsistemas/ControleMovimento.java
+++ lab05/subsistemas/ControleMovimento.java
@@ -1,7 +1,8 @@
 package subsistemas;
 
 import ambiente.*;
-import exceptions.ColisaoException;
+import exceptions.NaoAereoException;
+import exceptions.RoboDesligadoException;
 import robos.*;
 
 public class ControleMovimento {
@@ -12,10 +13,16 @@ public class ControleMovimento {
         this.roboControlado = r;
 
     }
-    public void mover(int delX, int delY, int delZ, Ambiente amb) throws ColisaoException{
-        int novaX = roboControlado.getX()+delX;
-        int novaY = roboControlado.getY()+delY;
-        int novaZ = roboControlado.getZ()+delZ;
-        amb.moverEntidade(roboControlado, novaX, novaY, novaZ);
+    public void mover(int delX, int delY, int delZ, Ambiente amb) throws RoboDesligadoException, NaoAereoException{
+        try{
+            int novaX = roboControlado.getX()+delX;
+            int novaY = roboControlado.getY()+delY;
+            int novaZ = roboControlado.getZ()+delZ;
+            amb.moverEntidade(roboControlado, novaX, novaY, novaZ);
+            roboControlado.moverPara(novaX, novaY, novaZ);
+        }catch (RoboDesligadoException e){
+
+        }catch(NaoAereoException e){}
+    
     }
 }
